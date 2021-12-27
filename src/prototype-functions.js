72e32fb2e346b2f3b35d27ba868d38e610af2cc8
davibaltar <davibaltarx@gmail.com>
2021-12-26 23:12:51 -0300

    Removing unused function

--- src/prototype-functions.js
+++ src/prototype-functions.js
@@ -1,14 +1,3 @@
 String.prototype.replaceAll = function (search, replacement) {
     return this.split(search).join(replacement);
 };
-
-String.prototype.getBetweenStrs = function (str1, str2) {
-    try {
-        if (this.split(str1)[1] && this.split(str1)[1].split(str2)[0]) {
-            return this.split(str1)[1].split(str2)[0];
-        }
-        return '';
-    } catch (err) {
-        return '';
-    }
-};
