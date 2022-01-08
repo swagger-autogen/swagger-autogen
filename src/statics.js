4fd41881b35fb6be3f69a3bf2c42a832b31a6121
davibaltar <davibaltarx@gmail.com>
2022-01-08 16:00:03 -0300

    Improvement

--- src/statics.js
+++ src/statics.js
@@ -21,7 +21,7 @@ const TEMPLATE = {
 
 const SWAGGER_TAG = '#swagger';
 const STRING_BREAKER = '__¬!@#$¬__'; // for line break and return without text changes
-const STRING_QUOTE = '__¬!@#_QUOTE_¬!@#__';
+const STRING_QUOTE = '__¬¬¬¬__QUOTE__¬¬¬¬__';
 const METHODS = ['get', 'head', 'post', 'put', 'delete', 'patch', 'options'];
 const RESERVED_FUNCTIONS = ['if', 'for', 'while', 'forEach'];
 module.exports = {
