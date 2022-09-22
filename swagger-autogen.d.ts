c07ebea56a2e1faba02f330840f8c5dabe189fef
davibaltar <davibaltarx@gmail.com>
2022-09-21 23:12:00 -0300

    TS files

---
+++ swagger-autogen.d.ts
@@ -0,0 +1,6 @@
+declare function swaggerAutogen (args?: any, endpointsFiles?: any, data?: any): (outputFile: any, endpointsFiles: any, data?: any) => Promise<false | {
+    success: boolean;
+    data: any;
+}>;
+
+export = swaggerAutogen;
