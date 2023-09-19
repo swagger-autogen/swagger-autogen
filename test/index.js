2a287bf42ced3fd5db20145280af0070f20a07b5
Davi Baltar <davibaltarx@gmail.com>
2023-09-19 20:54:20 -0300

    Removing unused code

--- test/index.js
+++ test/index.js
@@ -1,20 +1,7 @@
 
-const tape = require('tape')
-const _test = require('tape-promise').default
-const test = _test(tape)
-
 String.prototype.replaceAll = function (search, replacement) {
   return this.split(search).join(replacement);
 };
 
 require('./unit.test.utils')
 require('./unit.test.handle-data')
-
-
-
-// TODO:
-// stack0SymbolRecognizer
-// stackSymbolRecognizer
-// popFunction
-// getSwaggerComments
-
